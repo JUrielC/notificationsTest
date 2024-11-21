6030bb983441ad126e63d7b10d2ab3b9d2c630be
JUrielC <uri.jucv@gmail.com>
2024-11-21 11:01:08 -0600

    ./

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 const sendNotification = async () => {
-  
-        const API_KEY = '18c99af0-703b-460f-ada7-c18e8b4d154c';
-        const SERVER_KEY = 'f955794e-5305-4759-af6a-2e0f87770b3e'
-        const CAMPAIGN_ID = '1000065543';
-        console.log('Enviando notificación...');
-        console.log('API_KEY:', API_KEY);
-        console.log('SERVER_KEY:', SERVER_KEY);
-        try {
-            const response = await fetch(  `https://api.indigitall.com/v1/campaign?page=0&limit=0&applicationId=${API_KEY}` , {
 
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',/* 
-                    'Content-Type': 'application/json', */
-                    Authorization: `Bearer ${SERVER_KEY}`
-                }/*, 
-                body: JSON.stringify({ 
-                    name: "test Notification",
-                    enabled: false,  
-                 }), */
-            });
+    const API_KEY = '18c99af0-703b-460f-ada7-c18e8b4d154c';
+    const SERVER_KEY = 'f955794e-5305-4759-af6a-2e0f87770b3e'
+    const CAMPAIGN_ID = '1000065543';
+    console.log('Enviando notificación...');
+    console.log('API_KEY:', API_KEY);
+    console.log('SERVER_KEY:', SERVER_KEY);
+    try {
+        const response = await fetch(`https://api.indigitall.com/v1/campaign/${CAMPAIGN_ID}`, {
 
-            if (response.ok) {
-                const data = await response.json();
-                alert('Notificación enviada: ' + data.message);
-            } else {
-                const errorData = await response.json();
-                console.error('Error:', errorData);
-                console.error('Error:', response);
-                alert('Error al enviar la notificación: ' + errorData.message);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            alert('No se pudo enviar la notificación');
+            method: 'PUT',
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${API_KEY}`
+            },
+            body: JSON.stringify({
+                name: "pinkNotification"
+            }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            alert('Notificación enviada: ' + data.message);
+        } else {
+            const errorData = await response.json();
+            console.error('Error:', errorData);
+            console.error('Error:', response);
+            alert('Error al enviar la notificación: ' + errorData.message);
         }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('No se pudo enviar la notificación');
+    }
 
 }
